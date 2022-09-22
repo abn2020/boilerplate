6acd7353e9c79f66d192eb657dc11abda6de9aa6
Abner Martins <abner.09.martins@gmail.com>
2022-09-22 13:54:01 -0300

    boilerplate curso next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = {
+  nextConfig,
+  withPWA
+}
